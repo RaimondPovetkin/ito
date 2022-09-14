d794d9d63e22cb230aa155cbee29d25870f7d4ed
RaimondPovetkin <rajm.pov@gmail.com>
2022-09-14 23:58:29 +0300

    e

---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <v-btn
+      @click="foo"
+    >
+      click
+    </v-btn>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView',
+  methods:{
+    foo(){
+      this.$router.push('about')
+    }
+  }
+}
+</script>
